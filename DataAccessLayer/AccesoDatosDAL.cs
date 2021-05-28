48b55716f5ddc44d9493392e176fd441b21c54ea
admin <simbrecht@birt.eus>
2021-05-28 18:40:11 +0200

    plantilla cargada

---
+++ DataAccessLayer/AccesoDatosDAL.cs
@@ -0,0 +1,161 @@
+﻿using ObjetosTransferencia.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DataAccessLayer
+{
+    public static class AccesoDatosDAL
+    {
+        #region Atributos
+
+        private static String consultaSQL;
+
+        /// <summary>
+        ///  Cadena de conexion
+        /// </summary>
+        private static SqlConnection conexion = new SqlConnection("Data Source=localhost;Initial Catalog=Northwind;User ID=usuarioDI;Password=1234");
+
+        /// <summary>
+        /// lista de clientes
+        /// </summary>
+        private static List<ClienteDTO> listaClientes = new List<ClienteDTO>();
+
+        /// <summary>
+        /// Lista de pedidos de un cliente concreto
+        /// </summary>
+        private static List<PedidoDTO> listaPedidos = new List<PedidoDTO>();
+
+        #endregion
+
+        #region Constructores
+
+        #endregion
+
+        #region Propiedades
+
+        public static string ConsultaSQL { get => consultaSQL; set => consultaSQL = value; }
+        public static List<PedidoDTO> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
+        public static List<ClienteDTO> ListaClientes { get => listaClientes; set => listaClientes = value; }
+
+
+        #endregion
+
+        #region Metodos
+
+        /// <summary>
+        /// Consultar datos en una base de datos SQLServer 
+        /// </summary>
+        public static List<ClienteDTO> ListadoClientesNorthWind()
+        {
+            return RealizarConsultaClientes("select * from dbo.Customers");
+        }
+
+        /// <summary>
+        /// Pide a la base de datos la lista de pedidos para un cliente concreto
+        /// </summary>
+        /// <param name="indiceCliente"></param>
+        /// <returns></returns>
+        public static List<PedidoDTO> ListadoPedidosCliente(String nombreCliente)
+        {
+            return RealizarConsultaPedidos("select * from dbo.Orders WHERE CustomerID = '" + nombreCliente + "'");
+        }
+
+
+
+        /// <summary>
+        /// Realizar consulta a la BD
+        /// </summary>
+        private static List<ClienteDTO> RealizarConsultaClientes(String consulta)
+        {
+            SqlCommand command;
+
+            // Objeto para elctura de datos
+            SqlDataReader dataReader;
+
+            try
+            {
+                conexion.Open();
+
+                command = new SqlCommand(consulta, conexion);
+
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    // TODO: parsea los datos a la lista de ClientesDTO
+
+                }
+
+                dataReader.Close();
+                command.Dispose();
+
+                return listaClientes;
+
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se ha podido estabecer la conexion con la BD!" + e.Message);
+
+            }
+            finally
+            {
+                conexion.Close();
+
+            }
+
+        }
+
+
+        /// <summary>
+        /// Realizar consulta a la BD
+        /// </summary>
+        private static List<PedidoDTO> RealizarConsultaPedidos(String consulta)
+        {
+            SqlCommand command;
+
+            // Objeto para elctura de datos
+            SqlDataReader dataReader;
+
+            try
+            {
+                conexion.Open();
+
+                command = new SqlCommand(consulta, conexion);
+
+                dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    //TODO: creamos un pedido, parseamos y añadimos a la lista de clientes
+
+
+                }
+
+                dataReader.Close();
+                command.Dispose();
+
+                return listaPedidos;
+
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception("No se ha podido estabecer la conexion con la BD!" + e.Message);
+
+            }
+            finally
+            {
+                conexion.Close();
+
+            }
+
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ InterfazGrafica/UC/ListaPedidosUC.cs
@@ -0,0 +1,29 @@
+﻿using ObjetosTransferencia.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterfazGrafica.UC
+{
+    public partial class ListaPedidosUC : UserControl
+    {
+        private List<PedidoDTO> listaPedidos;
+
+        public ListaPedidosUC(String nombreCliente)
+        {
+            InitializeComponent();
+            // TODO: haz la peticion de los clientes y asociala a la lista y cargalo en el DataGridView
+         
+        }
+
+        public List<PedidoDTO> ListaPedidos { get => listaPedidos; set => listaPedidos = value; }
+
+
+    }
+}
